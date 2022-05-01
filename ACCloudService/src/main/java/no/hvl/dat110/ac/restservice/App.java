42fefb86454535d2bb4191418c5eee59cac95141
Kasper <591307@stud.hvl.no>
2022-05-01 14:15:10 +0200

    d

--- ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/App.java
+++ ACCloudService/src/main/java/no/hvl/dat110/ac/restservice/App.java
@@ -14,10 +14,10 @@ import com.google.gson.Gson;
  *
  */
 public class App {
-	
+
 	static AccessLog accesslog = null;
 	static AccessCode accesscode = null;
-	
+
 	public static void main(String[] args) {
 
 		if (args.length > 0) {
@@ -27,25 +27,54 @@ public class App {
 		}
 
 		// objects for data stored in the service
-		
+
 		accesslog = new AccessLog();
-		accesscode  = new AccessCode();
-		
+		accesscode = new AccessCode();
+
 		after((req, res) -> {
-  		  res.type("application/json");
-  		});
-		
+			res.type("application/json");
+		});
+
 		// for basic testing purposes
 		get("/accessdevice/hello", (req, res) -> {
-			
-		 	Gson gson = new Gson();
-		 	
-		 	return gson.toJson("IoT Access Control Device");
+
+			Gson gson = new Gson();
+
+			return gson.toJson("IoT Access Control Device");
+		});
+
+		// implement the routes required for the access control service
+		// as per the HTTP/REST operations describined in the project description
+
+		get("/accessdevice/log/", (req, res) -> {
+			return accesslog.toJson();
 		});
 		
-		// TODO: implement the routes required for the access control service
-		// as per the HTTP/REST operations describined in the project description
+		delete("/accessdevice/log/", (req, res) -> {
+			accesslog.clear();
+			return accesslog.toJson();
+		});
+
+		post("/accessdevice/log/", (req, res) -> {
+			AccessMessage am = new Gson().fromJson(req.body(), AccessMessage.class);
+			accesslog.add(am.getMessage());
+			return new Gson().toJson("OK");
+		});
+
+		get("/accessdevice/log/:id", (req, res) -> {
+			return new Gson().toJson(accesslog.get(Integer.parseInt(req.params(":id"))));
+		});
+
+		put("/accessdevice/code", (req, res) -> {
+			accesscode.setAccesscode(new Gson().fromJson(req.body(), AccessCode.class).getAccesscode());
+			return new Gson().toJson(accesscode);
+		});
+
+		get("/accessdevice/code", (req, res) -> {
+			return new Gson().toJson(accesscode);
+		});
+
 		
-    }
-    
+	}
+
 }
